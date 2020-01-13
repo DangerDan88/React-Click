4be69496e22feebd62bc82c73a963cb02bbbe0d9
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-01-13 14:42:26 -0700

    changed gitignore for build

---
+++ my-app/build/precache-manifest.67aa2fc44bd7d1276b19af020163ec3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabb6c3a48cea9846f4df8cc69dce513",
+    "url": "/React-Click/index.html"
+  },
+  {
+    "revision": "d439da8431056561d526",
+    "url": "/React-Click/static/css/main.abc25ff3.chunk.css"
+  },
+  {
+    "revision": "dc2286a20d0dc9de2cac",
+    "url": "/React-Click/static/js/2.b73811b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Click/static/js/2.b73811b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d439da8431056561d526",
+    "url": "/React-Click/static/js/main.b2392024.chunk.js"
+  },
+  {
+    "revision": "71abfd6c012cc11f280a",
+    "url": "/React-Click/static/js/runtime-main.c1abcfd7.js"
+  }
+]);
