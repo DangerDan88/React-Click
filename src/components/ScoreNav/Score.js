c8ad2f30b5a290ea75bef05a7f82bb6086b5b626
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-11-11 15:11:06 -0700

    text color was difficult to see

--- src/components/ScoreNav/Score.js
+++ src/components/ScoreNav/Score.js
@@ -4,18 +4,18 @@ import "./style.css";
 function ScoreNav(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <button className="navbar-brand text-white text-monospace" href="#">
+      <button className="navbar-brand text-black text-monospace" href="#">
         ScoreBoard
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <button className="nav-link text-white text-monospace" href="#">
+            <button className="nav-link text-black text-monospace" href="#">
               Score:{props.score}
             </button>
           </li>
           <li className="nav-item">
-            <button className="nav-link text-white text-monospace" href="">
+            <button className="nav-link text-black text-monospace" href="">
               Top Score:{props.topScore}
             </button>
           </li>
